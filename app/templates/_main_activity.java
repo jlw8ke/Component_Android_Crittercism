49eac75c98fe6048df67bab78ecb16b093fb5d52
Justin Washington <jwashington@mobiquityinc.com>
2014-11-06 14:46:26 -0500

    fixed another type in R.string.crittercism_app_id...sigh...

--- app/templates/_main_activity.java
+++ app/templates/_main_activity.java
@@ -1,2 +1,2 @@
 import com.crittercism.app.Crittercism;
-Crittercism.initialize(getApplicationContext(), getResources().getString(R.string.crittercism_app_id);
+Crittercism.initialize(getApplicationContext(), getResources().getString(R.string.crittercism_app_id));
